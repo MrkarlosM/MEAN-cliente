ce4fbc83740fc64e4ff78e380f26fcab390578ed
Carlos Mario Martínez <carlos.mario.m@outlook.com>
2023-01-22 09:31:55 -0500

    Add users

--- src/app/components/listar-productos/listar-productos.component.ts
+++ src/app/components/listar-productos/listar-productos.component.ts
@@ -1,10 +1,78 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { addDoc, doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore"; 
+import {app} from "../../../main";
 
 @Component({
   selector: 'app-listar-productos',
   templateUrl: './listar-productos.component.html',
   styleUrls: ['./listar-productos.component.css']
 })
+
+
+
 export class ListarProductosComponent {
+  db = getFirestore(app);
+  nombre="";
+  users="";
+  async action(){
+    try{const querySnapshot = await getDocs(collection(this.db, "mascotas"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+    });} 
+    catch(error){console.log(error)}
+  }
+
+  async crear(){
+    const citiesRef = collection(this.db, "cities");
+
+await setDoc(doc(citiesRef, "SF"), {
+    name: "San Francisco", state: "CA", country: "USA",
+    capital: false, population: 860000,
+    regions: ["west_coast", "norcal"] });
+await setDoc(doc(citiesRef, "LA"), {
+    name: "Los Angeles", state: "CA", country: "USA",
+    capital: false, population: 3900000,
+    regions: ["west_coast", "socal"] });
+await setDoc(doc(citiesRef, "DC"), {
+    name: "Washington, D.C.", state: null, country: "USA",
+    capital: true, population: 680000,
+    regions: ["east_coast"] });
+await setDoc(doc(citiesRef, "TOK"), {
+    name: "Tokyo", state: null, country: "Japan",
+    capital: true, population: 9000000,
+    regions: ["kanto", "honshu"] });
+await setDoc(doc(citiesRef, "BJ"), {
+    name: "Beijing", state: null, country: "China",
+    capital: true, population: 21500000,
+    regions: ["jingjinji", "hebei"] });
+  }
+
+async getDocCit(){
+  const docRef = doc(this.db, "cities", "SF");
+const docSnap = await getDoc(docRef);
+
+if (docSnap.exists()) {
+  const data = docSnap.data()
+  console.log("Document data:", data['name']);
+  this.nombre=data['name']
+} else {
+  // doc.data() will be undefined in this case
+  console.log("No such document!");
+}
+}
+
+async userdAdd() {
+  const docRef = await addDoc(collection(this.db, "users"), {
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e: any) {
+  console.error("Error adding document: ", e);
+}
 
 }
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,28 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+
+//Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBnJ1_xWUmRuUYhSC_SJC6HwrceX0ubcj4",
+  authDomain: "test-sdk-firebase.firebaseapp.com",
+  projectId: "test-sdk-firebase",
+  storageBucket: "test-sdk-firebase.appspot.com",
+  messagingSenderId: "854864800194",
+  appId: "1:854864800194:web:dfde8bed67f6dfe23dce5b",
+  measurementId: "G-2Q7HY2LP0C"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
